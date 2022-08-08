0ec1859631f42a4beb76da36e719da8d041aaed4
Jonathan Pope <jonathanmpope@gmail.com>
2022-08-08 11:07:06 -0600

    test: model test for plant plot find_item method

--- app/models/plant_plot.rb
+++ app/models/plant_plot.rb
@@ -2,7 +2,7 @@ class PlantPlot < ApplicationRecord
     belongs_to :plant
     belongs_to :plot
 
-    def self.find_item(params)
-        where("plant_id = ? AND plot_id = ?", params[:plant_id], params[:plot_id])
+    def self.find_item(data)
+        where("plant_id = ? AND plot_id = ?", data[:plant_id], data[:plot_id])
     end 
 end 
--- spec/models/plant_plot_spec.rb
+++ spec/models/plant_plot_spec.rb
@@ -5,4 +5,28 @@ RSpec.describe PlantPlot, type: :model do
     it {should belong_to :plant}
     it {should belong_to :plot}
   end
+
+   describe 'class methods' do
+    it 'returns a specific plant plot object' do  
+      garden1 = Garden.create!(name:"Pete's Fresh Things", organic:true)
+
+      plot1 = Plot.create!(number:10, size:"medium", direction:"North", garden_id:garden1.id)
+      plot2 = Plot.create!(number:5, size:"small", direction:"South", garden_id:garden1.id)
+      plot3 = Plot.create!(number:20, size:"large", direction:"East", garden_id:garden1.id)
+
+      plant1= Plant.create!(name:"SunFlower", description:"a large annual forb of the genus Helianthus grown as a crop for its edible oil and seeds", days_to_harvest:30)
+      plant2= Plant.create!(name:"Rose", description:"a woody perennial flowering plant of the genus Rosa", days_to_harvest:45)
+      plant3= Plant.create!(name:"Prickly Pear", description:"a genus of flowering plants in the cactus family Cactaceae", days_to_harvest:180)
+      
+      pp1 = PlantPlot.create!(plant:plant1, plot:plot1)
+      pp2 = PlantPlot.create!(plant:plant2, plot:plot1)
+      pp3 = PlantPlot.create!(plant:plant3, plot:plot1)
+      pp4 = PlantPlot.create!(plant:plant3, plot:plot2)
+      pp5 = PlantPlot.create!(plant:plant2, plot:plot2)
+      pp6 = PlantPlot.create!(plant:plant3, plot:plot3)
+
+      data = {:plant_id=>"#{plant1.id}", :plot_id=>"#{plot1.id}"}
+      expect(PlantPlot.find_item(data)[0]).to eq(pp1)
+    end 
+  end 
 end
